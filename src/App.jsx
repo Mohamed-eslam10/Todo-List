927186937c5a7e448d2f2155864f823cfabc9f0b
Mohamed eslam10 <mokee2008mes@gmail.com>
2026-03-22 11:12:40 +0200

    To do list is created

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useRef } from "react";
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef();
+
+  function addTodo() {
+    const text = inputRef.current.value;
+    const newItem = { completed: false, text };
+    if (text.trim() !== '') {
+
+      setTodos([...todos, newItem]);
+      // console.log(newItem);
+      inputRef.current.value = '';
+    }
+  };
+  function deletTodo(index) {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+    console.log(newTodos);
+  }
+  return (
+    <div className="bg-slate-300 h-[100vh] flex items-center justify-center ">
+
+      <div className="bg-white p-5 rounded-xl shadow-lg max-w-md lg:max-w-lg w-full">
+        <h1 className="font-bold text-2xl text-slate-800  border-b border-gray-200 text-center capitalize pb-3" >To do list</h1>
+        <div>
+          <ul className={todos.length > 0 ? "p-4 border-slate-200 border-b flex flex-col" : "hidden"}>
+            {todos.map((newItem, index) => {
+              return (
+                <div key={index}>
+                  <li onClick={() => { deletTodo(index) }} className={newItem.completed ? "line-through" : ""} >
+                    {newItem.text}
+                  </li>
+                </div>
+              );
+            })}
+            
+          </ul>
+          <div className="py-4 flex justify-between gap-2">
+            <input className="h-10 p-2 border w-full  border-slate-300 rounded-md focus:outline-none" type="text" placeholder="enter your mission " ref={inputRef} />
+            <button className="bg-blue-600 py-1 px-5 rounded-md text-white hover:bg-blue-700 cursor-pointer" onClick={addTodo}>Add</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
